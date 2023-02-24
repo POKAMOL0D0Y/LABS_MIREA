c35e3d8ec57a952173f555cc15c61b782daae6de
POKAMOL0D0Y <nikita.shvarts205148@mail.ru>
2023-02-24 23:51:45 +0300

    Create 2 activity

--- Project_start/app/src/main/java/com/example/project_start/MainActivity.java
+++ Project_start/app/src/main/java/com/example/project_start/MainActivity.java
@@ -1,69 +1,40 @@
 package com.example.project_start;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.widget.Toast;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "Ошибка";
 
-    @Override //запускается при запуске программы
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast TT=Toast.makeText(getApplicationContext(),"Программа начала работу, т.е. запуск активити!(onCreate)",Toast.LENGTH_LONG);
-        TT.show();
-        Log.i(TAG, "onCreate");
+        TextView text = (TextView)findViewById(R.id.Pharmacology);
+        String str = getResources().getString(R.string.app_name);
+        text.setText(str);
+
+        ImageView image = (ImageView)findViewById(R.id.imageView2);
+        Drawable draw = getResources().getDrawable(R.drawable.___________2);
+        image.setImageDrawable(draw);
+
+        //Button registration = (Button)findViewById(R.id.accepted);
+       // registration.setOnClickListener(new View.OnClickListener() {
+            //@Override
+            //public void onClick(View view) {
+                //Log.i(TAG, "Accepted");
+            //}
+        //});
     }
 
-    @Override //работает после восстановление
-    protected void onStart() {
-        super.onStart();
-        Toast TT=Toast.makeText(getApplicationContext(),"Программа снова запустилась (onStart)!",Toast.LENGTH_LONG);
-        TT.show();
-        Log.d(TAG, "onStart");
-    }
-
-    @Override //после функции пауза
-    protected void onResume() {
-        super.onResume();
-        Toast TT=Toast.makeText(getApplicationContext(),"Программа восстановила работу (onResume)!",Toast.LENGTH_SHORT);
-        TT.show();
-        Log.e(TAG, "onResume");
-    }
-
-    @Override // пауза
-    protected void onPause() {
-        super.onPause();
-        Toast TT=Toast.makeText(getApplicationContext(),"Программа поставилась на паузу (onPause)!",Toast.LENGTH_LONG);
-        TT.show();
-        Log.v(TAG, "onPause");
-    }
-
-    @Override //если человек вышел на рабочий стол и свернул приложение
-    protected void onStop() {
-        super.onStop();
-        Toast TT=Toast.makeText(getApplicationContext(),"Программа остановилась (onStop)!",Toast.LENGTH_LONG);
-        TT.show();
-        Log.w(TAG, "onStop");
-        if(isFinishing())
-            finish();
-    }
-
-    @Override // если человек зашел обратно в приложение
-    protected void onRestart() {
-        super.onRestart();
-        Toast TT=Toast.makeText(getApplicationContext(),"Программа перезапустилась (onRestart)!",Toast.LENGTH_LONG);
-        TT.show();
-        Log.i(TAG, "onRestart");
-    }
-
-    @Override //при закрытии приложения
-    protected void onDestroy() {
-        super.onDestroy();
-        Toast TT=Toast.makeText(getApplicationContext(),"Программа закончила работу! (onDestroy)",Toast.LENGTH_LONG);
-        TT.show();
-        Log.e(TAG, "onDestroy");
+    public void onMenuClick(View view) {
+        Log.i(TAG, "Accepted");
     }
 }
